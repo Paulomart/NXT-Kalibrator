f6c451aed843c1cc188bee059ddbce3bc359a66d
Paulomart <git@paul-h.de>
2016-01-19 13:13:05 +0100

    2. Winkel verkleinern

--- src/cool/paul/nxt/kalibrator/Kalibrator.java
+++ src/cool/paul/nxt/kalibrator/Kalibrator.java
@@ -22,8 +22,8 @@ public class Kalibrator {
 		Motor.A.rotate(320, true);
 		Motor.B.rotate(320);
 		//
-		Motor.A.rotate(-295, true);
-		Motor.B.rotate(295);
+		Motor.A.rotate(-290, true);
+		Motor.B.rotate(290);
 		//
 		Motor.A.rotate(1200, true);
 		Motor.B.rotate(1200);
